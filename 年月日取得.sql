72be7e1203eb36af4ee462f83e4c6b96f4d7b0c5
gogosub77 <ponta_gogo677@yahoo.co.jp>
2014-04-03 12:27:06 +0900

    Create 年月日取得.sql

---
+++ 年月日取得.sql
@@ -0,0 +1,24 @@
+/*切れ*/
+select * from t_conditionmn 
+where 
+term_date < convert(varchar,getdate(),111)
+order by term_date desc
+/*二週間*/
+select * from t_conditionmn 
+where 
+term_date >= convert(varchar,getdate(),111)
+and term_date <= convert(varchar,dateadd("d",14,getdate()),111)   
+order by term_date asc
+/*未到来*/
+select * from t_conditionmn 
+where 
+term_date > convert(varchar,dateadd("d",14,getdate()),111)   
+order by term_date asc
+
+
+--◆YYYY/MM/DD
+select convert(varchar,getdate(),111)
+--◆日付⇒YYYYMM
+select LEFT(convert(char(8),getdate(),112),6)
+--◆日数の足し算　YYYY/MM/DD
+select  convert(varchar,dateadd("d",14,getdate()),111)
