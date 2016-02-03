17162c8a6f7249990744c8f691e11bced76dbd39
takayanagi <ponta_gogo677@yahoo.co.jp>
2016-02-03 20:39:56 +0900

    tes

--- ストアドファイル出力echo.sql
+++ ストアドファイル出力echo.sql
@@ -1,10 +1,10 @@
-
+ï»¿
 DECLARE @cmd sysname, @var sysname;
 SET @var = 'Hello world>';
 	set @var= replace(@var,'>','^>')
 	set @var= replace(@var,'<','^<')
 
-SET @cmd = 'echo ' + @var + ' >> D:\‹¤—L\takayanagi_work\log2.txt'; 
+SET @cmd = 'echo ' + @var + ' >> D:\å…±æœ‰\takayanagi_work\log2.txt'; 
 
 
 --select @cmd
