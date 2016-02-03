00eecb9509782678556adf131313e2e266416411
takayanagi <ponta_gogo677@yahoo.co.jp>
2016-02-03 20:30:14 +0900

    ストアド

---
+++ ストアドファイル作成echo.sql
@@ -0,0 +1,11 @@
+
+DECLARE @cmd sysname, @var sysname;
+SET @var = 'Hello world>';
+	set @var= replace(@var,'>','^>')
+	set @var= replace(@var,'<','^<')
+
+SET @cmd = 'echo ' + @var + ' >> D:\‹¤—L\takayanagi_work\log2.txt'; 
+
+
+--select @cmd
+EXEC master..xp_cmdshell @cmd;
