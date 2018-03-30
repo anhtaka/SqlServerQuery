9c53606839a16ea177938c6ca1786dfda9f733ef
anhtaka <royal.norton@gmail.com>
2018-03-30 16:30:48 +0900

    Update 変更の追跡ONOFF.sql

--- 05.SQL/変更の追跡ONOFF.sql
+++ 05.SQL/変更の追跡ONOFF.sql
@@ -26,3 +26,10 @@ ALTER TABLE USER
 ENABLE CHANGE_TRACKING
 WITH (TRACK_COLUMNS_UPDATED = OFF);
 -------------------------------------------------
+
+
+[SQL Server の変更の追跡の機能を使用してみる](https://blog.engineer-memo.com/2015/01/22/sql-server-%E3%81%AE%E5%A4%89%E6%9B%B4%E3%81%AE%E8%BF%BD%E8%B7%A1%E3%81%AE%E6%A9%9F%E8%83%BD%E3%82%92%E4%BD%BF%E7%94%A8%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B/)
+
+SELECT * FROM [USER] AS s
+CROSS APPLY CHANGETABLE
+(VERSION [USER], ([1]), (s.[1])) AS c
